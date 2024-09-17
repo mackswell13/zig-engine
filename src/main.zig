92b30810e9d400b5471ee90818f96eda137d07eb
Maxwell Sherwin <msherwin@anchorgroup.tech>
2024-09-16 21:23:24 -0500

    rebase

--- src/main.zig
+++ src/main.zig
@@ -6,28 +6,35 @@ pub fn main() anyerror!void {
     const screenWidth = 800;
     const screenHeight = 450;
 
+    var pos_x: i32 = 40;
+    var pos_y: i32 = 40;
+
+
     rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
 
     defer rl.closeWindow(); // Close window and OpenGL context
 
     rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
 
     // Main game loop
-    while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        //----------------------------------------------------------------------------------
-
-        // Draw
-        //----------------------------------------------------------------------------------
-        rl.beginDrawing();
+    while (!rl.windowShouldClose()) { 
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.white);
 
-        rl.drawTriangle(rl.Vector2{ .x = 100, .y = 100}, rl.Vector2{ .x = 100, .y = 300 }, rl.Vector2{.x = 200, .y = 200 }, rl.Color.red);
-        //----------------------------------------------------------------------------------
+        if (rl.isKeyDown(rl.KeyboardKey.key_w)) {
+            pos_y -= 10;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_a)) {
+            pos_x -= 10;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_s)) {
+            pos_y += 10;
+        }
+        if (rl.isKeyDown(rl.KeyboardKey.key_d)) {
+            pos_x += 10;
+        }
+        rl.drawRectangle(pos_x, pos_y, 100, 100, rl.Color.red);
+
     }
 }
